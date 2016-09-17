e2fb62f08b4f262e7d43d5be9f30ea736a5ee781
Dagmara Szkurlat <daga.sz@hotmail.com>
2016-09-17 21:16:48 +0000

    Final typos

--- typos.rb
+++ typos.rb
@@ -28,6 +28,13 @@ def print(students)
   end
 end
 
+def print_footer students
+  puts "Overall, we have #{students.count} great students"
+end
+
+
+
 students = input_students
 print_header
 print(students)
+print_footer(students)
