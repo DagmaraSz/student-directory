daf9d96ec3cd456a114391699e3101098922e091
Dagmara Szkurlat <daga.sz@hotmail.com>
2016-09-17 21:05:48 +0000

    Excercises 9,10,12

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 def input_students
   puts "Please enter the name of the student"
   students = []
-  name = gets.chomp
+  name = gets.delete "\n"
   puts "what cohort? (optional - to skip hit enter)"
   month = gets.chomp.to_sym
   puts month
@@ -9,11 +9,11 @@ def input_students
 
   while !name.empty? do  
     students << {name: name, cohort: month, country: :Neverland, hobby: :mischief}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} student#{students.count != 1? 's':''}."
     puts "To finish the list, hit return twice"
     # get another name from the user
     puts "next name?"
-    name = gets.chomp
+    name = gets.delete "\n"
     puts "their cohort?"
     month = gets.chomp.to_sym
     month = :november if month.empty?
@@ -28,10 +28,14 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name][0].downcase == 'a' && student[:name].length < 12
-        puts "#{index+1}. #{student[:name].center(12)} (#{student[:cohort]} cohort) from #{student[:country]}. Hobby: #{student[:hobby]}"
-    end
+    if students.empty?
+        puts "No students :("
+    else
+      students.each_with_index do |student, index|
+        if student[:name][0].downcase == 'a' && student[:name].length < 12
+            puts "#{index+1}. #{student[:name].center(12)} (#{student[:cohort]} cohort) from #{student[:country]}. Hobby: #{student[:hobby]}"
+        end
+      end
   end
 end
 
@@ -40,20 +44,24 @@ def print_footer(students)
 end 
 
 def print_cohort(students)
-    #create a list (array) of the various cohorts in the student directory
-    cohort_months = []
-    students.each do |student|
-        unless cohort_months.include?(student[:cohort])
-            cohort_months << student[:cohort]
-        end
-    end
-    puts cohort_months
-    
-    #create an array of students in a given cohort
-    cohort_months.each do |cohort_month|
-        puts "The #{cohort_month} cohort: "
+    if students.empty?
+        puts "No students :("
+    else
+        #create a list (array) of the various cohorts in the student directory
+        cohort_months = []
         students.each do |student|
-            puts "#{student[:name]} (#{student[:cohort]} cohort) from #{student[:country]}. Hobby: #{student[:hobby]}" if student[:cohort] == cohort_month
+            unless cohort_months.include?(student[:cohort])
+                cohort_months << student[:cohort]
+            end
+        end
+        puts cohort_months
+        
+        #create an array of students in a given cohort
+        cohort_months.each do |cohort_month|
+            puts "The #{cohort_month} cohort: "
+            students.each do |student|
+                puts "#{student[:name]} (#{student[:cohort]} cohort) from #{student[:country]}. Hobby: #{student[:hobby]}" if student[:cohort] == cohort_month
+            end
         end
     end
 end
