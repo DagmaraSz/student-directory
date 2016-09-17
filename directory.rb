0cad50ac944742484918e196acee67bac65a20a5
Dagmara Szkurlat <daga.sz@hotmail.com>
2016-09-17 15:25:51 +0000

    Methoding

--- directory.rb
+++ directory.rb
@@ -12,18 +12,22 @@ students = [
   "Norman Bates"
 ]
 
-puts "The students of Random Academy"
-puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+def print_header
+  puts "The students of Random Academy"
+  puts "-------------"
+end
 
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end 
+
+
+print_header
+print(students)
+print_footer(students)
